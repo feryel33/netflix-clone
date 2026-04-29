7b88fa7f5e6cc3af218202b570a4c32b874f497f
feryel-w <elkamelferyel@gmail.com>
2026-04-30 00:58:03 +0100

    fix: exclude frontend from root eslint to avoid globals conflict

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 export default [
   {
+    ignores: ['frontend/**', 'node_modules/**', '__tests__/**'],
     rules: {
       'no-unused-vars': ['warn', { caughtErrorsIgnorePattern: '^_' }],
       'no-undef': 'warn'
