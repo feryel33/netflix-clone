46ef12c5ee46c5f8f62ea0f2882778ae597d358d
feryel-w <elkamelferyel@gmail.com>
2026-04-30 02:07:28 +0100

    fix: use /* instead of * for catch-all route

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use("/api/v1/search", protectRoute, searchRoutes);
 
 if (ENV_VARS.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../frontend/dist")));
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
   });
 }
