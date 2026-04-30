f5eeeb183448588974f91b199ce794a418e38835
feryel-w <elkamelferyel@gmail.com>
2026-04-30 02:02:31 +0100

    feat: serve frontend from backend for production deployment

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,35 @@
 import express from 'express';
-
-
 import authRoutes from './routes/auth.route.js';
 import movieRoutes from './routes/movie.route.js';
 import tvRoutes from './routes/tv.route.js';
 import { protectRoute } from './middleware/protectRoute.js';
 import searchRoutes from './routes/search.route.js';
-
-
-
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
 import cookieParser from 'cookie-parser';
-
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
-const PORT = ENV_VARS.PORT
+const PORT = ENV_VARS.PORT;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use(express.json()) 
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 
-app.use("/api/v1/auth", authRoutes)
-app.use("/api/v1/movie", protectRoute, movieRoutes)
-app.use("/api/v1/tv",protectRoute,tvRoutes)
-app.use("/api/v1/search",protectRoute,searchRoutes)
-
-
-
+if (ENV_VARS.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  });
+}
 
 app.listen(PORT, () => {
-    console.log('Server is  running at http://localhost:' + PORT);
-    connectDB();
-}); 
-
+  console.log('Server is running at http://localhost:' + PORT);
+  connectDB();
+});
